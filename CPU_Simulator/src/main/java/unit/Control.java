c817c71996e7a76d3667e98103a484029221fff1
oltenu <oldarius2002@gmail.com>
2023-11-09 15:36:39 +0200

    fixes on Control class

--- CPU_Simulator/src/main/java/unit/Control.java
+++ CPU_Simulator/src/main/java/unit/Control.java
@@ -20,7 +20,7 @@ public class Control implements Unit {
     private boolean memoryToRegister;
     private boolean registerWrite;
 
-    public Control(){
+    public Control() {
         units = new HashMap<>();
         registers = new HashMap<>();
         instruction = "000";
@@ -39,21 +39,21 @@ public class Control implements Unit {
         registerWrite = false;
         aluSource = false;
 
-        switch (instruction){
-            case "000":
+        switch (instruction) {
+            case "000": //R
                 registerDestination = true;
                 registerWrite = true;
                 aluOperation = "000";
 
                 break;
-            case "001":
+            case "001": //ADDI
                 extendedOperation = true;
                 aluSource = true;
                 registerWrite = true;
                 aluOperation = "100";
 
                 break;
-            case "010":
+            case "010": //LW
                 extendedOperation = true;
                 aluSource = true;
                 memoryToRegister = true;
@@ -61,33 +61,33 @@ public class Control implements Unit {
                 aluOperation = "100";
 
                 break;
-            case "011":
+            case "011": //SW
                 extendedOperation = true;
                 aluSource = true;
                 memoryWrite = true;
                 aluOperation = "100";
 
                 break;
-            case "100":
+            case "100": //BEQ
                 extendedOperation = true;
                 branch = true;
                 aluOperation = "001";
 
                 break;
-            case "101":
+            case "101": //ORI
                 aluSource = true;
                 registerWrite = true;
                 aluOperation = "101";
 
                 break;
-            case "110":
+            case "110": //SLTI
                 extendedOperation = true;
                 aluSource = true;
                 registerWrite = true;
                 aluOperation = "110";
 
                 break;
-            case "111":
+            case "111": //J
                 jump = true;
 
                 break;
@@ -96,7 +96,7 @@ public class Control implements Unit {
 
     @Override
     public void update() {
-        instruction = ((IfId) registers).getInstruction().getInstruction().substring(0, 3);
+        instruction = ((IfId) registers.get("IfId")).getInstruction().getInstruction().substring(0, 3);
     }
 
     @Override
